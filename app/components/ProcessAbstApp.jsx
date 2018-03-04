bedabf77b630845ae09383380285c9e80e05bd0b
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2018-03-04 19:39:47 -0300

    Estrutura do projeto

---
+++ app/components/ProcessAbstApp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ProcessAbstApp extends Component {
+  render() {
+    return (
+      <div id="content">
+        <h5>Time to <a href="https://facebook.github.io/react/">React</a>.</h5>
+      </div>
+    );
+  }
+}
---
+++ app/initialize.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import ProcessAbstApp from 'components/ProcessAbstApp';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    < ProcessAbstApp />,
+    document.getElementById('app')
+  );
+});
